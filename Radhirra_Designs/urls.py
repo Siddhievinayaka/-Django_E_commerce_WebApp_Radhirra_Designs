e512fbe88591f4850f68cb048464cd635adaae67
Siddhievinayaka <siddhievinayakakharaat5@gmail.com>
2025-11-26 12:29:38 +0530

    urls updated

--- Radhirra_Designs/urls.py
+++ Radhirra_Designs/urls.py
@@ -20,7 +20,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("Radhirra/", include("Radhirra.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("Radhirra/", include("Radhirra.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
