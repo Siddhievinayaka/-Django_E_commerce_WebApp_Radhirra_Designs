4f05cc36c3e7a1aa5ced0fe31b2e74aaa5a6c84b
Siddhievinayaka <siddhievinayakakharaat5@gmail.com>
2025-12-21 17:10:16 +0530

    Fix1.1 with Amazon Q

--- Radhirra_Designs/settings.py
+++ Radhirra_Designs/settings.py
@@ -112,6 +112,10 @@ DATABASES = {
         DATABASE_URL,
         conn_max_age=600,
         ssl_require=True,
+        conn_health_checks=True,
+        options={
+            'sslmode': 'require',
+        }
     )
 }
 
